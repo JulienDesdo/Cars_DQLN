ec8916b17237c4cf02198a92cbb4c0a8f0293efe
kdorey <kerian.dorey@gmail.com>
2025-01-20 12:17:08 +0100

    New Class car

--- main.py
+++ main.py
@@ -13,35 +13,60 @@ extensions = ['sphinx.ext.autodoc'] (inclusion des docsstrings, configuration de
 makehtml
 '''
 
+import pygame
 # from module import MyClass
+from vehicule_class import Vehicule
+import numpy as np
+
+class Carte:
+    """Exemple de classe dans module1.py."""
+
+    def __init__(self, size: list):
+        
+        self.size =  size
+
+    def method(self):
+        """Affiche un message avec le nom."""
+        print(f"Hello from {self.name} in MyClass1!")
+
+    def get_size(self):
+        return self.size
+
 
 def main():
     '''Point d'entrée principal du programme.'''
     # mon_obj = MyClass()
 
+    print("Start programme !")
 
+    pygame.init()
+    
+    BLEU = (0,0,150)
+    VERT = (0,150,0)
 
+    carte = Carte((1280,720))
+    screen = pygame.display.set_mode(carte.get_size())
+    clock = pygame.time.Clock()
+    running = True
+    car = Vehicule(1,1,0.,[np.linspace(0,np.pi,10)])
+    Rect_car = pygame.Rect(100,540,50,30)
+    Rect_Arrive = pygame.Rect(1100,50,50,50)
+
+    while running==True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running=False
+
+             
+        screen.fill("brown")
+        pygame.draw.rect(screen,BLEU,Rect_car)
+        pygame.draw.rect(screen,VERT,Rect_Arrive)
+
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
 
 if __name__ == "__main__" : 
     main()
-
-
-
-# Rappel sur les Classes en python, on les placera à la racine du projet. 
-'''
-class MyClass:
-    """Exemple de classe dans module1.py."""
-
-    def __init__(self, name: str):
-        """
-        Initialise l'instance de la classe.
-
-        Args:
-            name (str): Le nom à associer à cette instance.
-        """
-        self.name = name
-
-    def method(self):
-        """Affiche un message avec le nom."""
-        print(f"Hello from {self.name} in MyClass1!")
-'''
---
+++ vehicule_class.py
@@ -0,0 +1,40 @@
+
+
+class Vehicule:
+    """Class Véhicule qui va manipuler l'ensemble du véhicule."""
+
+    def __init__(self, vit: int,acc: int, orientation:float, Champ:list):
+        """
+        Initialise l'instance de la classe.
+        """
+        self.speed = vit
+        self.acceleration = acc
+        self.orientation = orientation
+        self.vision = Champ
+        self.position = [0,0]
+
+    def setSpeed(self,ns:int):
+        """Upgrade variable Speed with a new value"""
+        self.speed = ns
+
+
+    def setOrientation(self,no:int):
+        """Upgrade variable Orientation with a new value"""
+        self.orientation = no
+        
+    def setVision(self,nv:int):
+        """Upgrade variable Vision with a new value"""
+        self.speed = nv
+
+    def setPositionX(self):
+        """Upgrade variable X axe Position with a new value"""
+        self.position[0] = self.speed + self.position[0]
+
+    def setPositionX(self):
+        """Upgrade variable Y axes Position with a new value"""
+        self.position[1] = self.speed + self.position[1]
+
+
+
+
+
